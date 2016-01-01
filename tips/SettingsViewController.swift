cc2ec5ecc9614d5838861b9303b0554cc397554f
fuluoduo <fuluoduo@gmail.com>
2015-12-31 23:20:18 -0600

    Push

---
+++ tips/SettingsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by ao guo on 12/29/15.
+//  Copyright © 2015 ao guo. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func ValueChanged(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(tControl.selectedSegmentIndex, forKey: "savedint")
+        defaults.synchronize()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,16 +10,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
-
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+    var select = defaults.integerForKey("savedint")
+        tipControl.selectedSegmentIndex = select
+    }
+    override func viewDidDisappear(animated: Bool) {
+        let previousDate = NSDate.timeIntervalSinceReferenceDate()
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(Int(NSString(string: billField.text!).doubleValue), forKey: "previousAmount")
+        defaults.setDouble(previousDate, forKey: "previousTime")
+        defaults.synchronize()
+    }
+    override func viewDidAppear(animated: Bool) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        billField.becomeFirstResponder()
+        let preAmount = defaults.integerForKey("previousAmount")
+        let now = NSDate.timeIntervalSinceReferenceDate()
+        let preTime = defaults.doubleForKey("previousTime")
+        if (preAmount > 0 && (now - preTime) < (10 * 60)){
+            billField.text = "\(preAmount)"
+        }
+        else{
+            billField.text = ""
+        }
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tipPercentages = [0.18,0.2,0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        tipLabel.text = formatAmount(tip)
+        totalLabel.text = formatAmount(total)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func formatAmount (amount: Double) ->String{
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        formatter.locale = NSLocale.currentLocale()
+        return formatter.stringFromNumber(amount)!
+    }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        if (billField.text == ""){
+        UIView.animateWithDuration(1, animations: {
+            self.billField.frame = CGRectMake(132, 300, 168, 30)
+            print("Empty Bill")
+            self.tipLabel.hidden = true
+            self.totalLabel.hidden = true
+            self.tipControl.hidden = true
+                })
+        }
+        else{
+            UIView.animateWithDuration(1, animations: {
+                self.billField.frame = CGRectMake(132, 83, 168, 30)
+                self.tipLabel.hidden = false
+                self.totalLabel.hidden = false
+                self.tipControl.hidden = false
+            })
+        }
+        var tipPercentages = [0.18,0.2,0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        tipLabel.text = formatAmount(tip)
+        totalLabel.text = formatAmount(total)
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
 }
 
